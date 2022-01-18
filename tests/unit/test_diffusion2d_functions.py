fcf606fa8b276216d90745bcf663c4ae815dc61d
Mohamad Altaweel <mhd.altaweel@yahoo.com>
2022-01-18 02:06:26 +0100

    create tests

--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -2,6 +2,7 @@
 Tests for functions in class SolveDiffusion2D
 """
 
+from cv2 import solve
 from diffusion2d import SolveDiffusion2D
 
 
@@ -10,6 +11,11 @@ def test_initialize_domain():
     Check function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(20.,20.,0.01,0.01)
+    expected_nx = 2000
+    expected_ny = 2000
+    assert expected_nx == solver.nx
+    assert expected_ny == solver.ny
 
 
 def test_initialize_physical_parameters():
@@ -17,6 +23,11 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.dx = 0.1
+    solver.dy = 0.1
+    solver.initialize_physical_parameters(5.,200.,800.)
+    expected_dt = 0.0005
+    assert expected_dt == solver.dt
 
 
 def test_set_initial_condition():
